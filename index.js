6bc2c3aadb6fc01430a3155f72ff0ebb1d622a93
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-05-17 17:54:53 +0200

    add addevent listener

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
 
 const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (e) => {
+  //to prevent the form from refreshing
+  e.preventDefault();
+  const cityValue = cityInputEl.value;
+  console.log(cityValue);
+});
