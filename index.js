eff43dd3590ce539fddd9e94c527429fab70e493
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-05-17 17:49:24 +0200

    label html elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const apikey = "5075c715d1cbf033e72f0647294dd972";
+
+const weatherDataEl = document.getElementById("weather-data");
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
