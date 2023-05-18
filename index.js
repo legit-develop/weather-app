e8d8d60c48f18db6bf69b8c1c619a5adaae30c18
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-05-18 11:31:37 +0200

    add variable to api data results

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ async function getWeatherData(cityValue) {
     if (!response.ok) {
       throw new Error("NetWork response not ok");
     }
+    const data = await response.json();
+    console.log(data);
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}`,
+      `Wind speed: ${data.wind.speed}`,
+    ];
   } catch (error) {
     console.log(error);
   }
